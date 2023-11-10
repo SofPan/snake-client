adbc616f7d74cf5d0b861b6b5946955a397d4a1a
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-10 12:36:03 -0500

    add input.js and move setupInput and handleUserInput functions to this file

---
+++ input.js
@@ -0,0 +1,21 @@
+const stdin = process.stdin;
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+
+stdin.on("data", handleUserInput);
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,23 +1,5 @@
 const { connect } = require('./client');
-const stdin = process.stdin;
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-
-const handleUserInput = (key) => {
-  if (key === "\u0003") {
-    process.exit();
-  }
-};
-
-stdin.on("data", handleUserInput);
+const { setupInput } = require('./input');
 
 console.log("Connecting ...");
 connect();
