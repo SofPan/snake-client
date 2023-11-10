c25bfeac11ad8ea72ba1d37df23dde4ff4ab038c
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-10 11:53:42 -0500

    create client.js module and move connect function to this module

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // Let the user know they are connected
+  conn.on("connect", () => {
+    console.log("Connected...");
+  });
+
+  // Log data from server to the user
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,27 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  // Let the user know they are connected
-  conn.on("connect", () => {
-    console.log("Connected...");
-  });
-
-  // Log data from server to the user
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-  return conn;
-};
-
+const connect = require('./client');
 
 console.log("Connecting ...");
 connect();
