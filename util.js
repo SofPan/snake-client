b2d1bf0165a9d7045378180391242e4946a5e013
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-16 10:59:25 -0500

    add space to username prompt

--- util.js
+++ util.js
@@ -2,7 +2,7 @@ const prompt = require('prompt-sync')();
 
 // Prompt user for their name and write to server
 const getName = (connection) => {
-  const name = prompt('Enter your initials');
+  const name = prompt('Enter your initials ');
   connection.write(`Name: ${name}`);
 };
 
